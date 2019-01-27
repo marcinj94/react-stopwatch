682d874e15757c196cb1c6d0bafc6063fd50605f
beardofdoom <marcin.jusiak@gmail.com>
2019-01-27 14:07:01 +0100

    Updates

---
+++ precache-manifest.4e15fedfd3b1449bbafe9e9227b5cd61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dabf04ea5231d86e6037d2d2bbf81a05",
+    "url": "/react-stopwatch/static/media/bg2.dabf04ea.jpg"
+  },
+  {
+    "revision": "2d0941061b4bec27a492",
+    "url": "/react-stopwatch/static/js/runtime~main.2d094106.js"
+  },
+  {
+    "revision": "d818e8ff021e283eba4f",
+    "url": "/react-stopwatch/static/js/main.d818e8ff.chunk.js"
+  },
+  {
+    "revision": "8d19a6f060bc400734f1",
+    "url": "/react-stopwatch/static/js/1.8d19a6f0.chunk.js"
+  },
+  {
+    "revision": "d818e8ff021e283eba4f",
+    "url": "/react-stopwatch/static/css/main.9a52a632.chunk.css"
+  },
+  {
+    "revision": "94be681ba5f94c7d618b76444fa10d0b",
+    "url": "/react-stopwatch/index.html"
+  }
+];
